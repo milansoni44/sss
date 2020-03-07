1f8cb745d3a9b4a8bc79658e83fde5feac9f89e3
milansoni44 <milansoni44@gmail.com>
2020-03-07 19:21:40 +0530

    new

--- application/controllers/Payments.php
+++ application/controllers/Payments.php
@@ -10,83 +10,6 @@ class Payments extends MY_Controller {
         $this->debug = true;
     }
 
-    public function pay_membership_fee() {
-        if($this->input->server("REQUEST_METHOD") == "POST") {
-            $data = $this->input->post();
-            
-            if(!empty($data['user_id']) && !empty($data['membership_fee'])) {
-                $arrTransaction = [];
-                $user = $data['user_id'];
-                $fee = $data['membership_fee'];
-                for($index = 0; $index < count($data['user_id']); $index++) {
-                    $arrTransaction[$index] = [
-                        'user_id'               => $user[$index],
-                        'membership_fee_paid'   => $fee[$index],
-                        'created_at'            => date('Y-m-d H:i:s')
-                    ];
-                }
-
-                /* if($this->debug) {
-                    echo "<pre>"; 
-                    print_r($arrTransaction);
-                    echo "</pre>";
-                    die;
-                } */
-                $this->db->trans_start();
-                $this->db->insert_batch('transactions', $arrTransaction);
-                $this->db->trans_complete();
-
-                if($this->db->trans_status()) {
-                    $this->session->set_flashdata("success", "Membership fee paid successfully.");
-                } else {
-                    $this->session->set_flashdata("success", "Membership fee is failed to pay. Please try again later");
-                }
-            }
-        } else {
-            show_404();
-        }
-    }
-
-    public function add_membership_fee() {
-        /**
-         * get the members who do not pay the monthly membership fee 
-         */
-        $sql = "SELECT 
-                    user_master.*,
-                    month_member.membership_fee_paid
-                FROM user_master
-                LEFT JOIN (
-                    SELECT
-                        transactions.membership_fee_paid,
-                        transactions.user_id
-                    FROM transactions
-                    WHERE (MONTH(created_at) = '".date('m')."' AND YEAR(created_at) = '".date('Y')."')
-                ) AS month_member ON month_member.user_id = user_master.user_id
-                WHERE month_member.membership_fee_paid IS NULL
-                AND user_master.user_type <> 'Admin'
-                ";
-        
-        $resultArr = $this->db->query($sql)->result_array();
-        /*echo "<pre>";
-        echo $this->db->last_query();
-        echo "-----------------------------------------------------------";
-        echo "<br/>";
-        print_r($resultArr);
-        echo "</pre>";*/
-
-        $this->data['members'] = $resultArr;
-        $this->data['page_name'] = "Membership Fee Pay";
-		$this->data['breadcrumb'] = $this->load->view('payment/breadcrumb', $this->data, TRUE);
-        $this->data['jquery_view'] = $this->load->view('layout/jQuery', $this->data, TRUE);
-
-        $this->data['footer_panel'] = $this->load->view('layout/footer_panel', $this->data, TRUE);
-        $this->data['sidebar'] = $this->load->view('layout/sidebar', $this->data, TRUE);
-
-        $this->load->view('layout/header', $this->data);
-        $this->load->view('payment/membership_fee', $this->data);
-        $this->load->view('layout/footer', $this->data);
-    }
-
 	public function index()
 	{
         if($this->input->is_ajax_request()) {
@@ -183,162 +106,6 @@ class Payments extends MY_Controller {
         $this->load->view('layout/header', $this->data);
         $this->load->view('payment/payment_list', $this->data);
         $this->load->view('layout/footer', $this->data);
-    }
-    
-    public function generate_invoice() {
-
-        $active_members = $this->db->query("SELECT
-                                                user_id,
-                                                user_type,
-                                                DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(insert_date, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(insert_date, '00-%m-%d')) AS membership_years
-                                            FROM user_master
-                                            WHERE status = 'Active'
-                                            AND user_type <> 'Admin'
-                                            ")->result_array();
-                
-        // echo "<pre>"; 
-        // echo "Total Active Members :".count($active_members)."<br/>";
-        // print_r($active_members);die;
-
-        /*Get Total Inactive members of last 6 month*/
-        $demise_members = $this->db->query("SELECT
-                                                COUNT(*) AS total_demises,
-                                                GROUP_CONCAT(user_id) AS user_demises_ids
-                                            FROM user_master
-                                            WHERE status = 'Deactive'
-                                            AND user_type <> 'Admin'
-                                            AND inactivity_date BETWEEN '2019-07-01' AND '2020-03-31'
-                                            ")->row_array();
-
-        // print_r($demise_members);die;
-
-        if(!empty($active_members)) {
-            foreach($active_members as &$member) {
-                $member['demise_rate'] = ($demise_members['total_demises']*100);
-                $member['institute_rate'] = ($demise_members['total_demises']*30);
-                $member['administrative_rate_unit'] = (1*90);
-                $member['total_demise'] = $demise_members['total_demises'];
-                $member['demise_rate_unit'] = 100;
-                $member['institute_rate_unit'] = 30;
-                $member['demises_ids'] = $demise_members['user_demises_ids'];
-            }
-        }
-        // echo "<pre>";
-        // print_r($active_members);
-        // die;
-
-        if(!empty($active_members)) {
-            foreach($active_members as $transaction) {
-                // if($transaction['user_id'] != 3) continue;
-                $txtArr = explode(',', $transaction['demises_ids']);
-                if(!empty($txtArr)) {
-                    $institute = [
-                        'user_id'=>$transaction['user_id'],
-                        'amount'=>$transaction['institute_rate'],
-                        'ledger_id'=>3,
-                        'date_created'=>date('Y-m-d H:i:s'),
-                        'status'=> ($transaction['user_type'] != "Advance deposite") ? 'UNPAID' : 'PAID'
-                    ];
-                    // print_r($institute);
-                    if($transaction['user_type'] == 'Advance deposite') {
-                        $institute['payment_mode'] = 'Deposite';
-                    }
-                    $this->db->insert("transactions", $institute);
-                    if($transaction['user_type'] == "Advance deposite") {
-                        $this->db->set('balance', 'balance-'.$transaction['institute_rate'], false);
-                        $this->db->where('user_id' , $transaction['user_id']);
-                        $this->db->update('user_master');
-                    }
-                }
-
-                // print_r($demiseArr);
-                foreach($txtArr as $txn) {
-                    // check user membership year > 25 years
-                    if($transaction['membership_years'] < 25) {
-                        $demise = [
-                            'user_id'=>$transaction['user_id'],
-                            'amount'=>100,
-                            'demise_user_id'=>$txn,
-                            'date_created'=>date('Y-m-d H:i:s'),
-                            'status'=>($transaction['user_type'] != "Advance deposite") ? 'UNPAID' : 'PAID'
-                        ];
-                        if($transaction['user_type'] == 'Advance deposite') {
-                            $demise['payment_mode'] = 'Deposite';
-                        }
-                        // print_r($demise);
-                        $this->db->insert("transactions", $demise);
-                        if($transaction['user_type'] == "Advance deposite") {
-                            $this->db->set('balance', 'balance-'.$transaction['institute_rate'], false);
-                            $this->db->where('user_id' , $transaction['user_id']);
-                            $this->db->update('user_master');
-                        }
-                    }
-                    // print_r($demise);
-                }
-
-                $administrative = [
-                    'user_id'=>$transaction['user_id'],
-                    'amount'=>90,
-                    'ledger_id'=>1,
-                    'date_created'=>date('Y-m-d H:i:s'),
-                    'status'=>($transaction['user_type'] != "Advance deposite") ? 'UNPAID' : 'PAID'
-                ];
-                
-                if($transaction['user_type'] == 'Advance deposite') {
-                    $administrative['payment_mode'] = 'Deposite';
-                }
-                // print_r($administrative);
-                $this->db->insert("transactions", $administrative);
-                if($transaction['user_type'] == "Advance deposite") {
-                    $this->db->set('balance', 'balance-'.$transaction['institute_rate'], false);
-                    $this->db->where('user_id' , $transaction['user_id']);
-                    $this->db->update('user_master');
-                }
-            }
-        }
-        echo "Success";
-    }
-
-    public function interest_paid() {
-        // 3 monthly
-        // pay interest 4% in user account
-
-        $interests = $this->db->query("SELECT
-                            user_master.*
-                        FROM user_master
-                        WHERE user_type = 'Advance deposite'
-                        AND status = 'Active'
-                        ")->result_array();
-        // echo "<pre>";
-        // echo "Total Advance Deposite Members : ".count($interests)."<br/>";
-        // print_r($interests);
-
-        if(!empty($interests)) {
-            foreach($interests as $interest) {
-                $int_paid_amount = (float)(($interest['balance']*4)/100);
-                // echo "User_id : {$interest['user_id']} Balance : ".$interest['balance']." Interest Amount : {$int_paid_amount}<br/>";
-                $this->db->query("UPDATE user_master SET balance = {$interest['balance']}+{$int_paid_amount}");
-
-                $transactions = [
-                    'user_id'=>$interest['user_id'],
-                    'amount'=>$int_paid_amount,
-                    'ledger_id'=>3,
-                    'date_created'=>date('Y-m-d H:i:s'),
-                    'date_paid'=>date('Y-m-d H:i:s'),
-                    'payment_mode'=>'AUTO',
-                    'status'=>'PAID',
-                    'type'=>'Credit'
-                ];
-
-                $this->db->insert('transactions', $transactions);
-
-                // update institute ledger account
-                $this->db->set('balance', 'balance-'.$int_paid_amount, false);
-                $this->db->where('id' , 3);
-                $this->db->update('ledger');
-            }
-        }
-        echo "Success";die;
     }
 
     public function post_payment_individually() {
@@ -529,7 +296,7 @@ class Payments extends MY_Controller {
                             // echo "<pre>";
                             // echo "Unpaid Invoices Amount :.".$pending_payment[0]['total_amount']."<br/>";
                             // print_r($pending_payment);
-                            if($pending_payment[0]['total_amount'] > $amount) {
+                            if($pending_payment[0]['total_amount'] != $amount) {
                                 $errors[] = "Line No: ".$k." with data {$name} : Amount <b style='text-decoration: underline;'>".$amount."</b> is not same as pending.";
                             }
 
@@ -567,10 +334,10 @@ class Payments extends MY_Controller {
                         $errors_main = array_merge($errors_main,$errors);
                     }
                     //$this->pre($errors_main);
-                    if($insertRes > 0 && !empty($errors_main)) {
+                    if($insertCount > 0 && !empty($errors_main)) {
                         $this->session->set_flashdata("success","{$insertCount} records imported successfully.");
                         $this->session->set_flashdata('failure', $errors_main);
-                    } else if( $insertRes == 0 &&  !empty($errors_main)) {
+                    } else if( $insertCount == 0 &&  !empty($errors_main)) {
                         $this->session->set_flashdata('failure', $errors_main);
                     } else {
                         $this->session->set_flashdata("success","{$insertCount} records imported successfully.");
@@ -579,7 +346,7 @@ class Payments extends MY_Controller {
                 }else{
                     $this->session->set_flashdata("failure","Empty file can't be processed.");
                     redirect("payments", "location");
-                }
+                }//rakesh
             }else{
                 $this->session->set_flashdata("failure","Please try again later.");
                 redirect("payments", "location");
@@ -595,105 +362,5 @@ class Payments extends MY_Controller {
         $this->load->view('layout/header', $this->data);
         $this->load->view('payment/import_payment', $this->data);
         $this->load->view('layout/footer', $this->data);
-    }
-
-    public function send_invoice_email($user_id) {
-
-        $member = $this->db->where("user_id",$user_id)->get("user_master")->row_array();
-
-        $pending_payments = $this->db
-                                    ->query("SELECT
-                                        (
-                                            CASE 
-                                                WHEN transactions.ledger_id IS NOT NULL THEN ledger.name
-                                                WHEN transactions.demise_user_id IS NOT NULL THEN ud.name
-                                                ELSE NULL
-                                            END
-                                        ) AS ledger_name,
-                                        user_master.name as user_name,
-                                        transactions.id,
-                                        transactions.demise_user_id,
-                                        transactions.ledger_id,
-                                        transactions.amount,
-                                        transactions.date_created
-                                    FROM transactions
-                                    LEFT JOIN ledger ON ledger.id = transactions.ledger_id
-                                    LEFT JOIN user_master ud ON ud.user_id = transactions.demise_user_id
-                                    LEFT JOIN user_master ON user_master.user_id = transactions.user_id
-                                    WHERE transactions.user_id = '{$member['user_id']}'
-                                    AND transactions.status = 'UNPAID'")->result_array();
-        // echo $this->db->last_query();die;
-        // echo "<pre>"; print_r($pending_payments);die;
-        $this->data['member'] = $member;
-        $this->data['details'] = $pending_payments;
-        // echo "<pre>"; print_r($this->data['details']);die;
-        $this->data['payable_amount'] = ($pending_payments) ? array_sum(array_column($pending_payments,'amount')) : 0;
-
-        // $this->load->view('payment_pdf_template/invoice_header_only', $this->data);
-        // $this->load->view('payment_pdf_template/invoice', $this->data);
-        // $this->load->view('layout/footer', $this->data);
-
-        
-        $html ="";
-        $html .= $this->load->view('payment_pdf_template/invoice_header_only', $this->data, true);
-        $html .= $this->load->view('payment_pdf_template/invoice', $this->data, true);
-        $html .= $this->load->view('layout/footer', $this->data, true);
-        
-        // 1. Display PDF in browser
-        // $this->generate_pdf($html,"my_pdf_file.pdf");
-
-        // 2. Generate PDF and store in file system, so it can be used to send as email attachment.
-        $file_name = FCPATH.'Generated PDF'. DIRECTORY_SEPARATOR . "{$pending_payments[0]['user_name']}.pdf";
-        $this->generate_pdf($html,$file_name,"F");
-
-
-        // 3. Send PDF as email attachment
-        if(file_exists($file_name)){
-            $this->load->library('mymailer');
-            $attachment = array($file_name);
-            $email_response = $this->mymailer->send_email("Invoice","Please Find Attached Invoice",$member['email'],null,null,$attachment);
-            var_dump($email_response);
-            unlink($file_name);
-        }
-    }
-
-    protected function generate_pdf($html,$file_name=NULL,$mode='I'){
-
-        if($mode == 'F') {
-            $pathInfo = pathinfo($file_name);
-            if(isset($pathInfo['extension']) && $pathInfo['dirname']!='.'){
-
-                $dir_structure =dirname($file_name);
-                if (!file_exists($dir_structure)) {
-                    mkdir($dir_structure, 0777, true);
-                }
-            }
-        }
-
-        $modeArr = array(
-            'I'=>\Mpdf\Output\Destination::INLINE,
-            'D'=>\Mpdf\Output\Destination::DOWNLOAD,
-            'F'=>\Mpdf\Output\Destination::FILE,
-            'S'=>\Mpdf\Output\Destination::STRING_RETURN,
-        );
-
-        $mpdf = new \Mpdf\Mpdf(
-            array(
-                // 'mode' => 'utf-8',
-                // 'format' => array(210, 297),
-                // 'orientation' => 'P',
-                // 'setAutoTopMargin' => 'stretch',
-                // 'autoMarginPadding' => 0,
-                // 'bleedMargin' => 0,
-                // 'crossMarkMargin' => 0,
-                // 'cropMarkMargin' => 0,
-                // 'nonPrintMargin' => 0,
-                // 'margBuffer' => 0,
-                // 'collapseBlockMargins' => false,
-            )
-        );
-        $mpdf->SetDisplayMode('fullpage');
-        $mpdf->WriteHTML($html);
-        $mpdf->Output($file_name,$modeArr[$mode]);
     }
 }
